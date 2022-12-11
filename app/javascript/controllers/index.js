36951d1281548ad647c2a02a736c0b85f10a229e
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-12-11 14:45:36 +0100

    the moon today in the navbar

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import MoonController from "./moon_controller"
+application.register("moon", MoonController)
