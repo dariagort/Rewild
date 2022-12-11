50bf2e02161a271a3a838d765b39b79f62d80c6a
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-12-11 18:17:54 +0100

    routes ready

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,7 @@
 class Site < ApplicationRecord
   belongs_to :user
   has_many :logs
+  has_many :moods
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_11_102034) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_11_165050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_11_102034) do
     t.date "date"
     t.bigint "site_id", null: false
     t.index ["site_id"], name: "index_logs_on_site_id"
+  end
+
+  create_table "moods", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sites", force: :cascade do |t|
