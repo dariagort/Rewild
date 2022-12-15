7f5a6d56136d4978c73b37cd4601c392e88e52ac
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-12-15 04:59:22 +0100

    fixed timeline css/html and empty site show page

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,8 +1,8 @@
 class Site < ApplicationRecord
   belongs_to :user
-  has_many :logs
-  has_many :moods
-  has_many :seeds
+  has_many :logs, dependent: :destroy
+  has_many :moods, dependent: :destroy
+  has_many :seeds, dependent: :destroy
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 end
